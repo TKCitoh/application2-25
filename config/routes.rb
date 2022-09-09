951f12504f9d63a27ba1a36e2e85348d5bd3b5bd
application2-25 <tkci10.pooh@gmail.com>
2022-09-09 05:21:14 +0000

    【fix】rails error

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
   root to: "homes#top"
   get "home/about" => "homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
