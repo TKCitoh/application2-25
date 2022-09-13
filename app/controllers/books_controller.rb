f9b7998a66889c63c7e2079ae9fb174f682e606b
EC2 Default User <ec2-user@ip-172-31-18-63.ap-northeast-1.compute.internal>
2022-09-13 09:28:50 +0000

    【add】バリデーション機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,13 +5,20 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+    @user = current_user
+    @books = Book.all
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def index
-    @book = Book.all
+    @books = Book.all
+    @user = current_user
   end
 
   def show
@@ -20,6 +27,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @user = @book.user
+    if @book.update(book_params)
+       flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -30,6 +50,6 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:title,:body)
+    params.require(:book).permit(:title, :opinion)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_07_041212) do
+ActiveRecord::Schema.define(version: 2022_09_11_033038) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2022_09_07_041212) do
     t.string "title"
     t.string "body"
     t.integer "user_id"
+    t.string "opinion"
   end
 
   create_table "users", force: :cascade do |t|
