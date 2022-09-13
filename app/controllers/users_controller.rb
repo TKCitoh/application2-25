fce3801f005d4be9523a79050d5c2a89386aceee
application2-25 <tkci10.pooh@gmail.com>
2022-09-13 09:28:50 +0000

    【add】バリデーション機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
-    @user = User.all
+    @users = User.all
+    @user = current_user
   end
 
   def show
@@ -15,8 +16,12 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
    private
